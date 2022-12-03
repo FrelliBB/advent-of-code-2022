1ab20952d396d0e1cd1e8d63bf5b935a235d3902
Francesco Borg Bonaci <francesco.borgbonaci@marshmallow.co>
2022-12-03 20:16:25 +0000

    day 2 cleanup

--- src/main/java/com/francescoborgbonaci/adventofcode2022/Day2.java
+++ src/main/java/com/francescoborgbonaci/adventofcode2022/Day2.java
@@ -1,8 +1,12 @@
 package com.francescoborgbonaci.adventofcode2022;
 
 import com.francescoborgbonaci.InputUtils;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 import static com.francescoborgbonaci.adventofcode2022.Day2.Hand.PAPER;
 import static com.francescoborgbonaci.adventofcode2022.Day2.Hand.ROCK;
@@ -12,6 +16,37 @@ import static com.francescoborgbonaci.adventofcode2022.Day2.HandResult.LOSE;
 import static com.francescoborgbonaci.adventofcode2022.Day2.HandResult.WIN;
 
 public class Day2 {
+
+    record Result(int part1Score, int part2Score) {
+        public Result add(Result other) {
+            return new Result(part1Score + other.part1Score, part2Score + other.part2Score);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        final Result result = InputUtils.processFileAsLines("day2.txt", Day2::process);
+        System.out.println("Part 1: " + result.part1Score());
+        System.out.println("Part 2: " + result.part2Score());
+    }
+
+    public static Result process(final List<String> lines) {
+        return lines.stream()
+            .map(Day2::getResultForStrategy)
+            .reduce(new Result(0, 0), Result::add);
+    }
+
+    private static Result getResultForStrategy(final String strategy) {
+        final String[] strategyPart = strategy.split(" ");
+
+        final Hand opponentHand = Hand.parse(strategyPart[0]);
+        final Hand yourHand = Hand.parse(strategyPart[1]);
+        final Scoring part1Score = getScoring(s -> s.opponentHand() == opponentHand && s.playerHand() == yourHand).orElseThrow();
+
+        final HandResult result = HandResult.parse(strategyPart[1]);
+        final Scoring part2Score = getScoring(s -> s.opponentHand() == opponentHand && s.result() == result).orElseThrow();
+
+        return new Result(part1Score.calculateScore(), part2Score.calculateScore());
+    }
 
     private static final List<Scoring> SCORING = List.of(
         new Scoring(ROCK, ROCK, DRAW),
@@ -25,20 +60,20 @@ public class Day2 {
         new Scoring(SCISSORS, SCISSORS, DRAW)
     );
 
+    record Scoring(Hand playerHand, Hand opponentHand, HandResult result) {
+        public int calculateScore() {
+            return playerHand.getPoints() + result.getPoints();
+        }
+    }
+
+    @RequiredArgsConstructor
     enum Hand {
         ROCK(1),
         PAPER(2),
         SCISSORS(3);
 
+        @Getter
         private final int points;
-
-        Hand(final int points) {
-            this.points = points;
-        }
-
-        public int getPoints() {
-            return points;
-        }
 
         public static Hand parse(String value) {
             return switch (value) {
@@ -50,20 +85,14 @@ public class Day2 {
         }
     }
 
+    @RequiredArgsConstructor
     enum HandResult {
         WIN(6),
         DRAW(3),
         LOSE(0);
 
+        @Getter
         final int points;
-
-        HandResult(final int points) {
-            this.points = points;
-        }
-
-        public int getPoints() {
-            return points;
-        }
 
         public static HandResult parse(String value) {
             return switch (value) {
@@ -75,56 +104,8 @@ public class Day2 {
         }
     }
 
-    record Scoring(Hand playerHand, Hand opponentHand, HandResult result) {
-        public int calculateScore() {
-            return playerHand.getPoints() + result.getPoints();
-        }
-    }
-
-    public static void main(String[] args) throws Exception {
-        final var result = InputUtils.processFileAsLines("day2.txt", strings -> process(strings));
-        System.out.println("Part 1: " + result.part1Score());
-        System.out.println("Part 2: " + result.part2Score());
-    }
-
-    public static Result process(final List<String> lines) {
-        final var part1Score = lines.stream()
-            .map(s -> s.split(" "))
-            .map(hands -> {
-                final var opponentHand = Hand.parse(hands[0]);
-                final var yourHand = Hand.parse(hands[1]);
-                return getScoring(yourHand, opponentHand);
-            }).mapToInt(Scoring::calculateScore)
-            .sum();
-
-        final var part2Score = lines.stream()
-            .map(s -> s.split(" "))
-            .map(hands -> {
-                final var opponentHand = Hand.parse(hands[0]);
-                final var result = HandResult.parse(hands[1]);
-                return getScoring(opponentHand, result);
-            }).mapToInt(Scoring::calculateScore)
-            .sum();
-        return new Result(part1Score, part2Score);
-    }
-
-    private static Scoring getScoring(final Hand opponentHand, final HandResult result) {
-        return SCORING.stream()
-            .filter(scoring -> scoring.result() == result)
-            .filter(scoring -> scoring.opponentHand() == opponentHand)
-            .findAny()
-            .orElseThrow(() -> new IllegalStateException("No scoring found to [%s] vs. [%s]".formatted(result, opponentHand)));
-    }
-
-    public static Scoring getScoring(Hand playerHand, Hand opponentHand) {
-        return SCORING.stream()
-            .filter(scoring -> scoring.playerHand() == playerHand)
-            .filter(scoring -> scoring.opponentHand() == opponentHand)
-            .findAny()
-            .orElseThrow(() -> new IllegalStateException("No scoring found for [%s] vs. [%s]".formatted(playerHand, opponentHand)));
-    }
-
-    record Result(int part1Score, int part2Score) {
+    private static Optional<Scoring> getScoring(Predicate<Scoring> predicate) {
+        return SCORING.stream().filter(predicate).findAny();
     }
 
 }
