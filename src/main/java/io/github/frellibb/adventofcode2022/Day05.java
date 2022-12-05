07361bc9586b21a4890bea0ff1f67b446397d5b5
Francesco Borg Bonaci <francesco.borgbonaci@marshmallow.co>
2022-12-05 17:53:07 +0000

    day 5 part 2

--- src/main/java/io/github/frellibb/adventofcode2022/Day05.java
+++ src/main/java/io/github/frellibb/adventofcode2022/Day05.java
@@ -9,10 +9,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Stack;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static java.lang.Character.isAlphabetic;
+import static java.util.stream.IntStream.iterate;
+import static java.util.stream.IntStream.range;
 import static java.util.stream.IntStream.rangeClosed;
 
 public class Day05 implements Day {
@@ -22,62 +25,77 @@ public class Day05 implements Day {
     @Override
     public Result process(final List<String> lines) {
         final List<List<String>> cratesAndInstructions = ListUtils.splitListByPredicate(lines, String::isBlank);
-        final List<String> crateLines = cratesAndInstructions.get(0);
-        final List<String> instructionLines = cratesAndInstructions.get(1);
+        final var crateLines = cratesAndInstructions.get(0);
+        final List<Instruction> instructions = cratesAndInstructions.get(1).stream().map(Instruction::from).toList();
 
+        return new Result() {
+            @Override
+            public Object part1() {
+                return crateMover9000(parseCrateStacks(crateLines), instructions);
+            }
+
+            @Override
+            public Object part2() {
+                return crateMover9001(parseCrateStacks(crateLines), instructions);
+            }
+        };
+    }
+
+    private String crateMover9001(final Map<Integer, Stack<Character>> crates, final List<Instruction> instructions) {
+        instructions.forEach(instruction -> {
+            final var crateMoverStack = new Stack<Character>();
+            range(0, instruction.amount()).mapToObj(i -> crates.get(instruction.from()).pop()).forEach(crateMoverStack::push);
+            range(0, crateMoverStack.size()).forEach(i -> crates.get(instruction.to).push(crateMoverStack.pop()));
+        });
+
+        return getTopOfEachStack(crates);
+    }
+
+    private String crateMover9000(Map<Integer, Stack<Character>> crates, List<Instruction> instructions) {
+        instructions.forEach(instruction -> {
+            range(0, instruction.amount()).forEach(_unused -> {
+                final var crate = crates.get(instruction.from()).pop();
+                crates.get(instruction.to()).push(crate);
+            });
+        });
+
+        return getTopOfEachStack(crates);
+    }
+
+    private String getTopOfEachStack(final Map<Integer, Stack<Character>> crates) {
+        return crates.values().stream()
+            .map(Stack::peek)
+            .map(String::valueOf)
+            .collect(Collectors.joining());
+    }
+
+    private Map<Integer, Stack<Character>> parseCrateStacks(final List<String> crateLines) {
         final Map<Integer, Stack<Character>> crateStacks = new HashMap<>();
-        for (int crate = crateLines.size()-1; crate >= 0; crate--) {
-            final String crateLine = crateLines.get(crate);
-            final List<Character> crateElements = getCrateElements(crateLine);
-            rangeClosed(1, crateElements.size()).forEach(i -> {
+        iterate(crateLines.size() - 1, crate -> crate >= 0, crate -> crate - 1)
+            .mapToObj(crateLines::get)
+            .map(this::getCrateElements)
+            .forEach(crateElements -> rangeClosed(1, crateElements.size()).forEach(i -> {
                 if (!crateStacks.containsKey(i)) {
                     crateStacks.put(i, new Stack<>());
                 }
                 final Character codePoint = crateElements.get(i - 1);
                 if (isAlphabetic(codePoint)) {
-                    crateStacks.compute(i, (integer, characters) -> {
+                    crateStacks.computeIfPresent(i, (integer, characters) -> {
                         characters.push(codePoint);
                         return characters;
                     });
                 }
-            });
-        }
-
-        instructionLines.stream().map(Instruction::from).forEach(instruction -> {
-            for (int i = 0; i < instruction.amount(); i++) {
-                crateStacks.get(instruction.to()).push(crateStacks.get(instruction.from()).pop());
-            }
-        });
-
-        final var part1 = crateStacks.values().stream().map(characters -> characters.peek()).map(character -> String.valueOf(character))
-            .collect(Collectors.joining());
-
-        // 19 -> 1, 5, 9, 13, 17
-        // 15 -> 1, 5, 9, 13
-        // 11 -> 1, 5, 9
-        // 7 -> 1, 5
-        // 3 -> 1
-
-        return new Result() {
-            @Override
-            public Object part1() {
-                return part1;
-            }
-
-            @Override
-            public Object part2() {
-                return null;
-            }
-        };
+            }));
+        return crateStacks;
     }
 
     record Instruction(int amount, int from, int to) {
         public static Instruction from(String instructionLine) {
-            final var matchResult = INSTRUCTION_PATTERN.matcher(instructionLine);
+            final Matcher matchResult = INSTRUCTION_PATTERN.matcher(instructionLine);
             if (matchResult.matches()) {
-                final var amount = matchResult.group(1);
-                final var from = matchResult.group(2);
-                final var to = matchResult.group(3);
+                final String amount = matchResult.group(1);
+                final String from = matchResult.group(2);
+                final String to = matchResult.group(3);
                 return new Instruction(Integer.parseInt(amount), Integer.parseInt(from), Integer.parseInt(to));
             }
             throw new IllegalArgumentException(instructionLine);
--- src/test/java/io/github/frellibb/adventofcode2022/Day05Test.java
+++ src/test/java/io/github/frellibb/adventofcode2022/Day05Test.java
@@ -32,6 +32,6 @@ public class Day05Test extends AbstractDayTest {
 
     @Override
     Object expectedPart2Result() {
-        return null;
+        return "MCD";
     }
 }
