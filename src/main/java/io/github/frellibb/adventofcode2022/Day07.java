5d03a4297d23f7ee6551ae1fdf57873d418806a1
Francesco Borg Bonaci <francesco.borgbonaci@marshmallow.co>
2022-12-22 16:59:37 +0000

    records and pattern matching

--- src/main/java/io/github/frellibb/adventofcode2022/Day07.java
+++ src/main/java/io/github/frellibb/adventofcode2022/Day07.java
@@ -1,12 +1,17 @@
 package io.github.frellibb.adventofcode2022;
 
+import io.github.frellibb.adventofcode2022.Day07.Commands.Command;
+import io.github.frellibb.adventofcode2022.Day07.Commands.GoToDirectory;
+import io.github.frellibb.adventofcode2022.Day07.Commands.GoToRoot;
+import io.github.frellibb.adventofcode2022.Day07.Commands.ListedDirectory;
+import io.github.frellibb.adventofcode2022.Day07.Commands.ListedFile;
+import io.github.frellibb.adventofcode2022.Day07.FileSystem.Directory;
+import io.github.frellibb.adventofcode2022.Day07.FileSystem.File;
 import io.github.frellibb.core.Day;
 import io.github.frellibb.core.Result;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Day07 implements Day {
@@ -34,6 +39,7 @@ public class Day07 implements Day {
             .orElseThrow();
 
         return new Result() {
+
             @Override
             public Object part1() {
                 return part1;
@@ -46,8 +52,27 @@ public class Day07 implements Day {
         };
     }
 
+    private Directory parseRoot(List<String> lines) {
+
+        Directory root = new Directory("/", new ArrayList<>(), null);
+        final List<Commands.Command> commands = lines.stream().map(s -> switch (s) {
+            case "$ cd /" -> new GoToRoot();
+            case "$ cd .." -> new Commands.GoBack();
+            case "$ ls" -> new Commands.ListNodes();
+            case String st && s.startsWith("$ cd ") -> new GoToDirectory(st.split(" ")[2]);
+            case String st && st.startsWith("dir") -> new ListedDirectory(st.split(" ")[1]);
+            case String st -> new ListedFile(st.split(" ")[1], Long.parseLong(st.split(" ")[0]));
+        }).map(record -> (Command) record).toList();
+
+        for (final Command command : commands) {
+            root = command.update(root);
+        }
+
+        return new GoToRoot().update(root);
+    }
+
     private List<Directory> findAllDirectories(Directory root) {
-        final var dirs = new ArrayList<Directory>(List.of(root));
+        final var dirs = new ArrayList<>(List.of(root));
         root.children().forEach(node -> {
             if (node instanceof Directory directory) {
                 dirs.addAll(findAllDirectories(directory));
@@ -56,62 +81,90 @@ public class Day07 implements Day {
         return dirs;
     }
 
-    private Directory parseRoot(List<String> lines) {
-        Directory root = new Directory("/", new ArrayList<>(), null);
-        Directory current = root;
+    static class Commands {
+        sealed interface Command permits GoBack, GoToDirectory, GoToRoot, ListedDirectory, ListedFile, ListNodes {
+            Directory update(Directory pwd);
+        }
 
-        for (String line : lines) {
-            if (line.startsWith("$ cd")) {
-                if (line.equals("$ cd /")) {
-                } else if (line.equals("$ cd ..")) { // Move up
-                    current = current.parent;
-                } else {
-                    final var name = line.substring(5);
-                    current = (Directory) current.children.stream()
-                        .filter(c -> c.name().equals(name))
-                        .findFirst()
-                        .orElseThrow();
-                }
-            } else if (line.startsWith("$ ls")) {
-
-            } else if (line.startsWith("dir ")) {
-                current.children.add(new Directory(line.split(" ")[1], new ArrayList<>(), current));
-            } else {
-                var split = line.split(" ");
-                current.children.add(new File(split[1], Long.parseLong(split[0])));
+        record ListNodes() implements Command {
+            @Override
+            public Directory update(final Directory pwd) {
+                return pwd;
             }
         }
-        return root;
-    }
 
-    record Directory(String name, List<Node> children, Directory parent) implements Node {
-        public long calculateSize() {
-            return children.stream().map(child -> switch (child) {
-                case File f -> f.calculateSize();
-                case Directory dir -> dir.calculateSize();
-            }).reduce(0L, Long::sum);
+        record ListedFile(String name, long size) implements Command {
+
+            @Override
+            public Directory update(final Directory pwd) {
+                pwd.children().add(new File(name, size));
+                return pwd;
+            }
         }
 
-        @Override
-        public String toString() {
-            return "dir %s %s".formatted(name, children.stream().map(Node::name).collect(Collectors.toSet()));
+        record ListedDirectory(String name) implements Command {
+
+            @Override
+            public Directory update(final Directory pwd) {
+                final var directory = new Directory(name, new ArrayList<>(), pwd);
+                pwd.children().add(directory);
+                return pwd;
+            }
+        }
+
+        record GoBack() implements Command {
+
+            @Override
+            public Directory update(final Directory pwd) {
+                return pwd.parent();
+            }
+        }
+
+        record GoToDirectory(String dir) implements Command {
+
+            @Override
+            public Directory update(final Directory pwd) {
+                return (Directory) pwd.children().stream().filter(node -> node.name().equals(dir)).findFirst().orElseThrow();
+            }
+        }
+
+        record GoToRoot() implements Command {
+
+            @Override
+            public Directory update(final Directory pwd) {
+                Directory root = pwd;
+                while (root.parent() != null) {
+                    root = root.parent();
+                }
+                return root;
+            }
         }
     }
 
-    record File(String name, long size) implements Node {
-        public long calculateSize() {
-            return size;
+    static class FileSystem {
+        record Directory(String name, List<Node> children, Directory parent) implements Node {
+            private long calculateSize() {
+                return children.stream().map(child -> switch (child) {
+                    case File f -> f.size();
+                    case Directory dir -> dir.calculateSize();
+                }).reduce(0L, Long::sum);
+            }
+
+            public String toString() {
+                return "dir %s %s".formatted(name, children.stream().map(Node::name).collect(Collectors.toSet()));
+            }
         }
 
-        @Override
-        public String toString() {
-            return "%s %d".formatted(name, size);
+        record File(String name, long size) implements Node {
+            public String toString() {
+                return "%s %d".formatted(name, size);
+            }
+        }
+
+        sealed interface Node permits File, Directory {
+
+            String name();
         }
     }
 
-    sealed interface Node permits File, Directory {
-        long calculateSize();
-
-        String name();
-    }
 }
