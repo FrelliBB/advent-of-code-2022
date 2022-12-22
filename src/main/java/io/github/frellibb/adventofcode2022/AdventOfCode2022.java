eb5b60a71c29fce6e5ff506a1b4f5ad06a3afc60
Francesco Borg Bonaci <francesco.borgbonaci@marshmallow.co>
2022-12-22 13:54:37 +0000

    handle ClassNotFoundException to skip unimplemented days

--- src/main/java/io/github/frellibb/adventofcode2022/AdventOfCode2022.java
+++ src/main/java/io/github/frellibb/adventofcode2022/AdventOfCode2022.java
@@ -6,19 +6,22 @@ import org.apache.commons.lang3.StringUtils;
 
 public class AdventOfCode2022 {
 
-    private static final int CURRENT_DAY = 6;
-
     public static void main(String[] args) throws Exception {
 
-        for (int i = 1; i <= CURRENT_DAY; i++) {
+        for (int i = 1; i <= 25; i++) {
             final var dayString = StringUtils.leftPad(Integer.toString(i), 2, "0");
-            final Class<?> clazz = Class.forName(AdventOfCode2022.class.getPackageName() + ".Day" + dayString);
-            final Day dayInstance = (Day) clazz.getDeclaredConstructor().newInstance();
+            final var className = AdventOfCode2022.class.getPackageName() + ".Day" + dayString;
+            try {
+                final Class<?> clazz = Class.forName(className);
+                final Day dayInstance = (Day) clazz.getDeclaredConstructor().newInstance();
 
-            final var lines = InputUtils.loadLines("day%s.txt".formatted(dayString));
+                final var lines = InputUtils.loadLines("day%s.txt".formatted(dayString));
 
-            System.out.println(clazz.getSimpleName());
-            System.out.println(dayInstance.process(lines).print());
+                System.out.println(clazz.getSimpleName());
+                System.out.println(dayInstance.process(lines).print());
+            } catch (ClassNotFoundException ex) {
+                System.out.printf("Skipping %s due to ClassNotFoundException%n", className);
+            }
         }
     }
 }
