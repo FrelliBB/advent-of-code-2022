28137e27ebfd3e90b33a67265ae6ebcf39d17b63
Francesco Borg Bonaci <francesco.borgbonaci@marshmallow.co>
2023-12-01 17:50:18 +0000

    day 1

--- src/main/java/io/github/frellibb/adventofcode/y2023/Day01.java
+++ src/main/java/io/github/frellibb/adventofcode/y2023/Day01.java
@@ -9,6 +9,18 @@ import java.util.List;
 public class Day01 implements Day {
     @Override
     public Result process(final List<String> lines) {
-        return new BasicResult(null, null);
+        final Integer result = lines.stream()
+            .map(Day01::processLine)
+            .map(Integer::parseInt)
+            .reduce(0, Integer::sum);
+
+        return new BasicResult(result, null);
+    }
+
+    private static String processLine(final String line) {
+        final String digitsOnly = line.replaceAll("[^0-9]", "");
+        final char firstDigit = digitsOnly.charAt(0);
+        final char lastDigit = digitsOnly.charAt(digitsOnly.length() - 1);
+        return "%s%s".formatted(firstDigit, lastDigit);
     }
 }
